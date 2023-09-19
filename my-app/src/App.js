bc44d0952a2e2bcaac50feb0219344bb466c2608
Camelia03 <camelia_weber@yahoo.com>
2023-09-19 16:52:59 +0000

    Filter a search challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import css from './App.module.css';
 import Sidebar from './components/Sidebar';
 import NavBarForm from './components/NavBarForm';
 import Content from './components/Content';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <NavBarForm />
       <Sidebar />
       <Content />
+      <SearchBar/>
     </div>
   );
 }
