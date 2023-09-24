5447d40d805f44a947cf96f8ebde3e5791eeba5c
Camelia03 <camelia_weber@yahoo.com>
2023-09-24 14:49:18 +0000

    Network Requests Challenge part 2: Converting Class component to using an API

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,7 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar';
 import NavBarForm from './components/NavBarForm';
-import Content from './components/Content';
-import SearchBar from './components/SearchBar';
+import ContentAPI from './components/ContentAPI';
 
 function App() {
   return (
@@ -10,8 +9,7 @@ function App() {
       {/* Add your components here */}
       <NavBarForm />
       <Sidebar />
-      <Content />
-      <SearchBar/>
+      <ContentAPI />
     </div>
   );
 }
